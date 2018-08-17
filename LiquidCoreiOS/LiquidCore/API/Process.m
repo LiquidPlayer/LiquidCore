cc9c8b907745939affb242eb7fd450763a6d5947
Eric Lange <eric@flicket.tv>
2018-08-17 16:39:41 +0200

    Added license notices

--- LiquidCoreiOS/LiquidCore/API/Process.m
+++ LiquidCoreiOS/LiquidCore/API/Process.m
@@ -2,14 +2,40 @@
 //  Process.m
 //  LiquidCoreiOS
 //
-//  Created by Eric Lange on 7/7/18.
-//  Copyright © 2018 LiquidPlayer. All rights reserved.
-//
+/*
+ Copyright (c) 2018 Eric Lange. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ - Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ - Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import <Foundation/Foundation.h>
 #import "Process.h"
 #import "NodeBridge.h"
 #import "FileSystem.h"
+#import <mach-o/dyld.h>
+
+extern int jsc_get_major_version (void);
+extern int jsc_get_minor_version (void);
+extern int jsc_get_micro_version (void);
 
 @interface BlockWrap : NSObject
 @property (readonly) Process* process;
@@ -176,7 +202,6 @@ static void onNodeExit(void *data, int code)
             [[self.context globalObject] deleteProperty:@"v8"];
             
             // Set correct 'process.config' values
-
 #if TARGET_OS_SIMULATOR
             const NSString *arch = @"x64";
 #else
@@ -193,6 +218,19 @@ static void onNodeExit(void *data, int code)
 #endif
             [self context][@"process"][@"config"][@"variables"][@"host_arch"] = arch;
             [self context][@"process"][@"config"][@"variables"][@"target_arch"] = arch;
+            
+            // Remove v8 version, since it is only the V8 API
+            [[self context][@"process"][@"versions"] deleteProperty:@"v8"];
+            
+            // Expose JavaScriptCore version
+            NSString *version = [[[NSBundle bundleForClass:JSValue.class
+                                   ] infoDictionary] objectForKey:@"CFBundleVersion"];
+            [self context][@"process"][@"versions"][@"jsc"] = version;
+
+            // Expose LiquidCore version
+            NSString *lcversion = [[[NSBundle bundleForClass:Process.class] infoDictionary]
+                                   objectForKey:@"CFBundleShortVersionString"];
+            [self context][@"process"][@"versions"][@"liquidcore"] = lcversion;
 
             [self eventOnStart:[self context]];
             [self setHold_:nil];
