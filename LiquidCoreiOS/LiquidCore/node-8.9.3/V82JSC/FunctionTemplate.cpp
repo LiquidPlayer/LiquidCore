5273d406d57f6ce22489a67d90498f2d098496f9
Eric Lange <eric@flicket.tv>
2019-01-01 14:40:30 +0100

    Oops

--- LiquidCoreiOS/LiquidCore/node-8.9.3/V82JSC/FunctionTemplate.cpp
+++ LiquidCoreiOS/LiquidCore/node-8.9.3/V82JSC/FunctionTemplate.cpp
@@ -4,7 +4,7 @@
  * Distributed under the MIT License.  See LICENSE.md at
  * https://github.com/LiquidPlayer/LiquidCore for terms and conditions.
  */
-include "V82JSC.h"
+#include "V82JSC.h"
 #include "JSObjectRefPrivate.h"
 
 using namespace v8;
