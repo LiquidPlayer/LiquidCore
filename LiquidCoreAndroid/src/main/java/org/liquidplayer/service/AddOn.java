ed2950c37aef737d04bfed72dbe0c36e95097747
Eric Lange <eric@flicket.tv>
2019-02-08 10:17:47 +0100

    Corrected AddOn documentation

--- LiquidCoreAndroid/src/main/java/org/liquidplayer/service/AddOn.java
+++ LiquidCoreAndroid/src/main/java/org/liquidplayer/service/AddOn.java
@@ -4,12 +4,24 @@ import org.liquidplayer.javascript.JSValue;
 
 /**
  * Supported addons must expose a public {@code Object} that implements this interface.  The
- * object must have a default constructor.
+ * object must expose a public constructor that takes {@code android.content.Context} as
+ * its lone parameter.
+ * <br><br>
+ * The package and naming of this class is extremely important.  The class *must* be in
+ * the {@code org.liquidplayer.addon} package, and it must be named the same as the native
+ * module that will be requested by JavaScript, with the following exceptions:
+ * <br><br>
+ * 1. The first character must be capitalized<br>
+ * 2. Any non-alphanumeric or underscore ('_') characters must be ommitted and the *next*
+ *    character capitalized.
+ * <br><br>
+ * For example, if node attempts to require "my-native-moDULE.node", LiquidCore will look for
+ * the class {@code org.liquidplayer.addon.MyNativeModULE} to resolve the addon.
  */
 public interface AddOn {
     /**
      * When {@code require('<MODULE_NAME>.node')} is called from JavaScript, this method will
-     * be triggered if {@code <MODULE_NAME>} is returned in {@code module()}.  This method
+     * be triggered if {@code <MODULE_NAME>} resolves to this class as described above.  This method
      * must then (1) load any required native libraries and (2) execute the
      * {@code __register_<MODULE_NAME>()} static function created from {@code NODE_MODULE_*} macros
      * in {@code node.h}.
